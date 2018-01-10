e6853dfef2fcf59ca7aa408766d4deaa09b0bd40
matski98 <34057263+matski98@users.noreply.github.com>
2018-01-10 18:19:14 +0100

    Add files via upload

--- zadanie1.c
+++ zadanie1.c
@@ -1,38 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct elem {
-	struct elem *nastepny;
-	int wartosc;
-} el;
-
-el *pierwszy;
-
-void dodaj (el *lista, int liczba){
-	el *wskaznik, *nowy;
-	wskaznik = lista;
-	while (wskaznik->nastepny != NULL)
-	{
-		wskaznik = wskaznik->nastepny;
-	}
-	nowy =(el*) malloc (sizeof(el));
-	nowy->wartosc = liczba;
-	nowy->nastepny = NULL;
-	wskaznik->nastepny = nowy;
-}
-void wypisz(el *lista){
-	el *wskaznik=lista;
-	while(wskaznik != NULL){
-		printf ("%d\n", wskaznik->wartosc);
-		wskaznik = wskaznik->nastepny;
-	}
-}
 int main(){
-	pierwszy =(el*) malloc (sizeof(el));
-	pierwszy->wartosc = 2;
-	pierwszy->nastepny = NULL;
-	int x=0;
-	dodaj(pierwszy, 3);
-	wypisz(pierwszy);
-	return 0;
+	int funkcja(){
+	struct Punkt{
+		float x;
+		float y;
+		float z;
+		char nazwa;
+		}
+		printf("wpisz wspolrzedne A");
+		scanf("%f", Punkt.x);
+		scanf("%f", Punkt.y);
+		
+	}
+return 0;
 }
